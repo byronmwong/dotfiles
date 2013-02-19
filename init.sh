f8220cb6294be3e738597e4cc08fe748a8fa5f21
Patrick Stankard <me@patrickstankard.com>
2013-02-19 16:00:43 -0500

    don't install rake

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-PREREQS="zsh git vim rake"
+PREREQS="zsh git vim"
 TIMESTAMP=$(date +%s)
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 [[ brew ]] && brew install $PREREQS
-[[ apt-get ]] && sudo apt-get install $PREREQS
-[[ yum ]] && sudo yum install $PREREQS
+[[ apt-get ]] && sudo apt-get install $PREREQS rake
+[[ yum ]] && sudo yum install $PREREQS rake
 
 chsh -s /bin/zsh
 
